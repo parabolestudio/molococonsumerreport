ee524b0c534d0b2f092614751a8fc28e2945d716
Kristin Baumann <hello@kristin-baumann.com>
2025-07-03 15:11:04 +0200

    Worked on viz 11

--- js/vis11.js
+++ js/vis11.js
@@ -1,81 +1,119 @@
 import { html, useEffect, useState } from "./utils/preact-htm.js";
 
-export function Vis11() {
-  const [genreData, setGenreData] = useState([]);
-  const [genderData, setGenderData] = useState([]);
-  const [demographicsData, setDemographicsData] = useState([]);
-  const [appData, setAppData] = useState([]);
+const URL =
+  "https://raw.githubusercontent.com/parabolestudio/molococonsumerreport/refs/heads/main/data/";
 
-  const URL =
-    "https://raw.githubusercontent.com/parabolestudio/molococonsumerreport/refs/heads/main/data/";
-  const fileName1 = "Viz11_1_genre_overview.csv";
-  const fileName2 = "Viz11_2_gender.csv";
-  const fileName3 = "Viz11_3_demographics.csv";
-  const fileName4 = "Viz11_4_top_apps.csv";
+function setCountryDropdownOptions(countries, selectedCountry, callback) {
+  // set values for country code dropdown
+  // const countries = ["Australia", "Germany"];
+  const uniqueCountries = Array.from(new Set(countries));
+  let countryDropdown = document.querySelector("#viz11_dropdown_countries");
+  if (countryDropdown) {
+    if (countryDropdown) countryDropdown.innerHTML = "";
+    uniqueCountries.forEach((country) => {
+      let option = document.createElement("option");
+      option.text = country;
+      countryDropdown.add(option);
+    });
+    countryDropdown.value = selectedCountry;
+    countryDropdown.addEventListener("change", (e) => {
+      callback(e);
+    });
+  }
+}
+function setCategoryDropdownOptions(categories, selectedCategory, callback) {
+  const uniqueCategories = Array.from(new Set(categories));
+  let categoryDropdown = document.querySelector("#viz11_dropdown_categories");
+  if (categoryDropdown) {
+    if (categoryDropdown) categoryDropdown.innerHTML = "";
+    uniqueCategories.forEach((country) => {
+      let option = document.createElement("option");
+      option.text = country;
+      categoryDropdown.add(option);
+    });
+    categoryDropdown.value = selectedCategory;
+    categoryDropdown.addEventListener("change", (e) => {
+      callback(e);
+    });
+  }
+}
+
+export function Vis11() {
+  const [selectedCountry, setSelectedCountry] = useState("Australia");
+  const [selectedCategory, setSelectedCategory] = useState("Travel");
+  const [genreData, setGenreData] = useState([]);
 
   useEffect(() => {
-    Promise.all([
-      d3.csv(URL + fileName1),
-      d3.csv(URL + fileName2),
-      d3.csv(URL + fileName3),
-      d3.csv(URL + fileName4),
-    ]).then(function (files) {
-      console.log("Files loaded:", files);
-      const [file1, file2, file3, file4] = files;
+    Promise.all([d3.csv(URL + "Viz11_1_genre_overview.csv")]).then((files) => {
+      const [genreDataFile] = files;
+      setGenreData(genreDataFile);
+
+      // update country dropdown options
+      const uniqueCountries = Array.from(
+        new Set(genreDataFile.map((d) => d.Country))
+      );
+      setCountryDropdownOptions(uniqueCountries, selectedCountry, (e) => {
+        setSelectedCountry(e.target.value);
+      });
+
+      // update category dropdown options
+      const uniqueCategories = Array.from(
+        new Set(genreDataFile.map((d) => d.Genre))
+      );
+      setCategoryDropdownOptions(uniqueCategories, selectedCategory, (e) => {
+        setSelectedCategory(e.target.value);
+      });
     });
   }, []);
 
-  // // Fetch data on mount
-  // useEffect(() => {
-  //   d3.csv(
-  //     "https://raw.githubusercontent.com/parabolestudio/molococonsumerreport/refs/heads/main/data/Viz12_time_spent_during_day.csv"
-  //   ).then((data) => {
-  //     //   data.forEach((d) => {
-  //     //     d["Value"] = +d["Value"];
-  //     //     d["Year"] = +d["Year"];
-  //     //   });
-
-  //     setData(data);
-  //   });
-  // }, []);
-
-  // if (data.length === 0) {
-  //   return html`<div>Loading...</div>`;
-  // }
-
-  const vis11Container = document.querySelector("#vis11");
-  const width =
-    vis11Container && vis11Container.offsetWidth
-      ? vis11Container.offsetWidth
-      : 600;
-
   return html`<div class="vis-container-inner">
-    <${Vis11Top} />
-    <${Vis11Bottom} />
+    <${Vis11Top}
+      genreData=${genreData}
+      selectedCountry="${selectedCountry}"
+      selectedCategory="${selectedCategory}"
+    />
+    <${Vis11Bottom}
+      selectedCountry="${selectedCountry}"
+      selectedCategory="${selectedCategory}"
+    />
   </div>`;
 }
 
-const Vis11Top = (props) => {
-  const pillList = ["Gaming", "RMG", "Content"];
+const Vis11Top = ({ genreData, selectedCategory, selectedCountry }) => {
+  if (!genreData || genreData.length === 0) {
+    return html`<div>Loading...</div>`;
+  }
 
-  const dotCount1 = 27;
-  const dotCount2 = 15;
+  const filteredGenreData = genreData.filter(
+    (d) => d.Country === selectedCountry && d.Genre === selectedCategory
+  );
+  const numberApps = filteredGenreData[0]["Apps used in the past 30 days"];
+  const numberAdOpportunities =
+    filteredGenreData[0]["Ad opportunity every 24h"];
+
+  const pillList = [
+    filteredGenreData[0]["Top Ad Opportunity #1"],
+    filteredGenreData[0]["Top Ad Opportunity #2"],
+    filteredGenreData[0]["Top Ad Opportunity #3"],
+  ];
 
   return html`<div class="vis-11-part vis-11-top">
-    <p class="title">Category Name</p>
+    <p class="title">${selectedCategory}</p>
     <div class="vis-11-top-grid">
       <div class="element1">
-        <p class="number">88</p>
+        <p class="number">${numberApps}</p>
         <p class="label">IAE apps used</p>
         <p class="sublabel">in the past 30 days</p>
       </div>
-      <div class="element2"><${DotSection} dotCount="${dotCount1}" /></div>
+      <div class="element2"><${DotSection} dotCount="${numberApps}" /></div>
       <div class="element3">
-        <p class="number">27</p>
+        <p class="number">${numberAdOpportunities}</p>
         <p class="label">Ad opportunities</p>
         <p class="sublabel">in the past 24 hours</p>
       </div>
-      <div class="element4"><${DotSection} dotCount="${dotCount2}" /></div>
+      <div class="element4">
+        <${DotSection} dotCount="${numberAdOpportunities}" />
+      </div>
       <div class="element5">
         <p class="label">Top ad opportunities</p>
       </div>
@@ -95,32 +133,80 @@ const DotSection = ({ dotCount }) => {
   </div>`;
 };
 
-const Vis11Bottom = () => {
-  const genderData = [
-    {
-      group: "Women",
-      value: 19,
-    },
-    {
-      group: "Men",
-      value: 81,
-    },
-  ];
+const Vis11Bottom = ({ selectedCountry, selectedCategory }) => {
+  const [rawGenderData, setRawGenderData] = useState([]);
+  const [rawAgeData, setRawAgeData] = useState([]);
+  const [genderData, setGenderData] = useState([]);
+  const [ageData, setAgeData] = useState([]);
 
-  const ageData = [
-    {
-      group: "18-34",
-      value: 50,
-    },
-    {
-      group: "35-54",
-      value: 41,
-    },
-    {
-      group: "55+",
-      value: 9,
-    },
-  ];
+  function prepData() {
+    // prep gender data
+    const copyRawGenderData = JSON.parse(JSON.stringify(rawGenderData));
+    let preppedGenderData = copyRawGenderData.filter(
+      (d) => d.Country === selectedCountry && d.Genre === selectedCategory
+    );
+    preppedGenderData.forEach((d) => {
+      d["group"] = d["Demographics (Gender)"];
+      delete d["Demographics (Gender)"];
+
+      d["value"] = +d["%"] * 100;
+      delete d["%"];
+      delete d["Country"];
+      delete d["Country code"];
+      delete d["Genre"];
+    });
+    setGenderData(preppedGenderData);
+
+    // prep age data
+    const copyRawAgeData = JSON.parse(JSON.stringify(rawAgeData));
+    let preppedAgeData = copyRawAgeData.filter(
+      (d) => d.Country === selectedCountry && d.Genre === selectedCategory
+    );
+    preppedAgeData.forEach((d) => {
+      d["group"] = d["Demographics (Age)"];
+      delete d["Demographics (Age)"];
+
+      d["value"] = +d["%"] * 100;
+      delete d["%"];
+      delete d["Country"];
+      delete d["Country code"];
+      delete d["Genre"];
+    });
+    setAgeData(preppedAgeData);
+  }
+
+  useEffect(() => {
+    const fileName2 = "Viz11_2_gender.csv";
+    const fileName3 = "Viz11_3_demographics.csv";
+    Promise.all([d3.csv(URL + fileName2), d3.csv(URL + fileName3)]).then(
+      (files) => {
+        let [genderDataFile, ageDataFile] = files;
+
+        setRawGenderData(genderDataFile);
+        setRawAgeData(ageDataFile);
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    if (
+      rawGenderData.length > 0 &&
+      rawAgeData.length > 0 &&
+      selectedCountry &&
+      selectedCategory
+    ) {
+      prepData();
+    }
+  }, [rawGenderData, rawAgeData, selectedCountry, selectedCategory]);
+
+  if (
+    !genderData ||
+    !ageData ||
+    genderData.length === 0 ||
+    ageData.length === 0
+  ) {
+    return html`<div>Loading...</div>`;
+  }
 
   const genderTemplateColumns = genderData.map((d) => `${d.value}%`).join(" ");
   const genderGridElements = genderData.map(
@@ -128,7 +214,7 @@ const Vis11Bottom = () => {
         <p class="sublabel">${d.group}</p>
       </div>
       <div style="grid-area: 2 / ${i + 1} / 3 / ${i + 1};" class="bar">
-        <p class="number">${d.value}%</p>
+        <p class="number">${Math.round(d.value)}%</p>
       </div>`
   );
   const ageTemplateColumns = ageData.map((d) => `${d.value}%`).join(" ");
@@ -137,7 +223,7 @@ const Vis11Bottom = () => {
         <p class="sublabel">${d.group}</p>
       </div>
       <div style="grid-area: 2 / ${i + 1} / 3 / ${i + 1};" class="bar">
-        <p class="number">${d.value}%</p>
+        <p class="number">${Math.round(d.value)}%</p>
       </div>`
   );
 
@@ -153,7 +239,9 @@ const Vis11Bottom = () => {
       </div>
     </div>
     <div>
-      <p class="label">Who uses the apps</p>
+      <p class="label">
+        Who uses the apps ${selectedCountry} ${selectedCategory}
+      </p>
       <div
         class="vis-11-grid-gender"
         style="grid-template-columns: ${genderTemplateColumns};"
