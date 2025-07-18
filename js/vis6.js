eff42f5a6df6425e8496ff9d24c8dcc8063e0733
Fernando Becerra <becerrafernando@gmail.com>
2025-07-18 16:00:07 -0300

    use data from github

--- js/vis6.js
+++ js/vis6.js
@@ -34,8 +34,7 @@ export function Vis6() {
   // Fetch data on mount
   useEffect(() => {
     d3.csv(
-      // "https://raw.githubusercontent.com/parabolestudio/molococonsumerreport/refs/heads/main/data/Viz6_time_on_mobile.csv"
-      "./data/Viz6_time_on_mobile.csv"
+      "https://raw.githubusercontent.com/parabolestudio/molococonsumerreport/refs/heads/main/data/Viz6_time_on_mobile.csv"
     ).then((data) => {
       data.forEach((d) => {
         d["Value"] = +d["Value"];
