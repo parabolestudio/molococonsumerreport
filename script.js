1f037bf684f28f64c04a1a5fcc0718cbbb67bf51
Kristin Baumann <hello@kristin-baumann.com>
2025-08-04 21:20:37 +0200

    Viz 3: remove from script

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import { html, renderComponent } from "./js/utils/preact-htm.js";
 
 import { Vis1 } from "./js/vis1.js";
 import { Vis2 } from "./js/vis2.js";
-import { Vis3 } from "./js/vis3.js";
+// import { Vis3 } from "./js/vis3.js";
 import { Vis4Combined } from "./js/vis4.js";
 import { Vis6, Vis6LegendGrowth } from "./js/vis6.js";
 import { Vis7 } from "./js/vis7.js";
@@ -30,10 +30,10 @@ const visList = [
     id: "vis2",
     component: Vis2,
   },
-  {
-    id: "vis3",
-    component: Vis3,
-  },
+  // {
+  //   id: "vis3",
+  //   component: Vis3,
+  // },
   {
     id: "vis4",
     component: Vis4Combined,
